ccc54fc5be571fdaf197a6fc0d6395db2d83d625
ChaminieDesilva <e16070@eng.pdn.ac.lk>
2021-08-05 22:51:45 +0530

    menu component added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
 import { Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar dark color = "primary">
+    <div>
+      <Navbar navbar-light color = "dark">
         <div className="container">
           <NavbarBrand href="/">
             Ristorante Con Fusion
           </NavbarBrand>
         </div>
       </Navbar>
+      <Menu/>
     </div>
   );
 }
