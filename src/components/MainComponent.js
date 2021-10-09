d4f99d904b7a922696c3749673d6355bd54dc9c5
ChaminieDesilva <e16070@eng.pdn.ac.lk>
2021-10-10 00:19:29 +0530

    added about page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ConactComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -9,6 +10,7 @@ import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
+import DishDetail from './DishDetailComponent';
 
 class Main extends Component {
 
@@ -33,13 +35,24 @@ class Main extends Component {
             promotion={this.state.promotions.filter((promo) => promo.featured)[0]}/>
         )
     }
+
+    const DishWithId=({match}) => {
+        return(
+            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+            comments= {this.state.comments.filter((comment) => comment.dishId=== parseInt(match.params.dishId, 10)) }
+             />
+        );
+    }
+
     return (
       <div>
         <Header/>
         <Switch>
             <Route path = "/home" component = {HomePage}/>
             <Route exact path = "/menu" component = {() => <Menu dishes = {this.state.dishes}/>} />
+            <Route path= "/menu/:dishId" component = {DishWithId}/>
             <Route exact path = "/contactus" component = {Contact} />
+            <Route exact path = "/aboutus" component = {() => <About leaders = {this.state.leaders}/>}/>
             <Redirect to = "/home"/>
         </Switch>
         <Footer/>
